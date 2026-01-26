9916c5d9ddd6aeda9d39e1f0a8accbd894f91198
Argenis Ramírez <ramirezd4557@gmail.com>
2026-01-26 09:31:52 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const navbar = document.querySelector('.navbar')
+const openMenu = document.querySelector('.open-menu')
+const closeMenu = document.querySelector('.close-menu')
+const links = document.querySelectorAll('.link')
+const overlay = document.querySelector('.overlay')
+
+// Función para activar el Menú Hamburgesa
+const toggleMenu = function () {
+
+    navbar.classList.toggle('active');
+    overlay.classList.toggle('active');
+
+}
+
+// Eventos para abrir y cerrar el Menú Hamburgesa
+openMenu.addEventListener('click', toggleMenu)
+closeMenu.addEventListener('click', toggleMenu)
+overlay.addEventListener('click', toggleMenu)
+
+links.forEach(function (link) {
+
+    link.addEventListener('click', toggleMenu)
+
+})
+
+//Fecha del Copyright Automática
+const date = document.querySelector('#date')
+date.innerHTML = new Date().getFullYear()
+
+//Mantener Barra de Navegación y Botón Scroll Arriba fijados
+const header = document.querySelector('.header')
+const topBtn = document.querySelector('.top-btn')
+
+window.addEventListener('scroll', function () {
+
+    if (this.scrollY > 100) {
+        header.classList.add('active')
+        topBtn.classList.add('active')
+    } else {
+        header.classList.remove('active')
+        topBtn.classList.remove('active')
+    }
+
+})
